13afaa5f5a6f6e7d926ec2379af8d46ee17d41f6
adailton-320 <adailtonjr320@gmail.com>
2024-05-11 23:22:56 -0300

    adiciona classe listaTarefa e seus metodos adiciona classe Tarefa e seus metodos

---
+++ Colecao-Java-Santander/src/colecaoList/ListaTarefa.java
@@ -0,0 +1,40 @@
+package colecaoList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+	private List<Tarefa> tarefaList;
+
+	public ListaTarefa() {
+		this.tarefaList = new ArrayList<Tarefa>();
+	}
+
+	public void adicionarTarefa(String descricao) {
+		tarefaList.add(new Tarefa(descricao));
+	}
+	
+	public void removerTarefa(String descricao) {
+		List<Tarefa>tarefaRemover= new ArrayList<Tarefa>();
+		 
+		for ( Tarefa t : tarefaList) {
+			
+			if(t.getDescricao().equalsIgnoreCase(descricao)) {
+				
+				tarefaRemover.add(t);
+				
+			}
+			tarefaList.removeAll(tarefaRemover);
+		}
+	}
+	
+	public int obterTotalTarefa() {
+		return tarefaList.size();
+	}
+	
+	public void obterDescricaoTarefa() {
+		System.out.println(tarefaList);
+	}
+
+}
---
+++ Colecao-Java-Santander/src/colecaoList/Tarefa.java
@@ -0,0 +1,17 @@
+package colecaoList;
+
+public class Tarefa {
+	
+	private String descricao;
+
+	
+	public Tarefa(String descricao) {
+		this.descricao= descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+
+}
